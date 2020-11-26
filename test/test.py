3dc8929b6d59e6952c5532b608ff1c995f335703
changxin W <986798607@qq.com>
2020-11-26 20:13:11 +0800

    test

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@
 # @Email   : 986798607@qq.com
 # @Software: PyCharm
 # @License: BSD 3-Clause
+from tqdm import tqdm
 
 if __name__ == "__main__":
     from sklearn.datasets import load_boston
@@ -22,7 +23,7 @@ if __name__ == "__main__":
     ###
 
     X, y = load_boston(return_X_y=True)
-    X = X[:, :5] #(简化计算，示意)
+    X = X[:, :5]  # (简化计算，示意)
     searchspace_list = [
         np.arange(0.01, 1, 0.1),
         np.array([0, 20, 30, 50, 70, 90]),
@@ -33,12 +34,12 @@ if __name__ == "__main__":
 
     searchspace = search_space(*searchspace_list)
 
-    spilt = 3 # 1. 划分3部分空间,在内存没有超的情况下，数字越小越好（>2）
-    sps = np.array_split(searchspace,spilt)
+    spilt = 5  # 1. 划分分空间,在内存没有超的情况下，数字越小越好（>2）
+    sps = np.array_split(searchspace, spilt)
 
     ms = []
 
-    for i in range(spilt): #2 .每一部分空间算出均值方差，然后合并
+    for i in tqdm(range(spilt), desc="空间"):  # 2 .每一部分空间算出均值方差，然后合并
 
         me = Ego(regclf=model, searchspace=sps[i], X=X, y=y, number=50, n_jobs=6)
 
@@ -49,5 +50,5 @@ if __name__ == "__main__":
 
     ms = np.concatenate(ms, axis=0)
 
-    me = Ego(regclf=model, searchspace = searchspace, X=X, y=y)  # 没什么用，只是需要全searchspace，最后的表格能对齐
-    re = me.egosearch(meanstd=ms) #3 . 用合并的均值方差算EI
+    me = Ego(regclf=model, searchspace=searchspace, X=X, y=y)  # 没什么用，只是需要全searchspace，最后的表格能对齐
+    re = me.egosearch(meanstd=ms)  # 3 . 用合并的均值方差算EI
